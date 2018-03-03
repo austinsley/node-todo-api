0e142b0a7240081b8a9cbdecfec61597136c724a
Austin Tinsley <austin1@live.unc.edu>
2018-03-03 01:00:20 -0500

    Refactored most of server.js

--- server/server.js
+++ server/server.js
@@ -1,33 +1,6 @@
-const mongoose = require('mongoose');
-
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
-
-var Todo = mongoose.model('Todo', {
-  text: {
-    type: String,
-    required: true,
-    minlength: 1,
-    trim: true
-  },
-  completed: {
-    type: Boolean,
-    default: false
-  },
-  completedAt: {
-    type: Number,
-    default: null
-  }
-});
-
-var User = mongoose.model('User', {
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    minlength: 1
-  }
-});
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
 // var otherTodo = new Todo({
 //   text: 'Eat dinner',
