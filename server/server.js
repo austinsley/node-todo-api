5a12e9e822b52cca6bfbbf33eef71f497be67976
Austin Tinsley <austin1@live.unc.edu>
2018-03-05 19:40:12 -0500

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-const express = require('express');
+const _ = require('lodash');
 const bodyParser = require('body-parser');
+const express = require('express');
 const {ObjectID} = require('mongodb');
 
 const {mongoose} = require('./db/mongoose');
@@ -66,6 +67,34 @@ app.delete('/todos/:id', (req, res) => {
     res.send({todo});
   }).catch((e) => {
     res.status(400).send(e);
+  });
+});
+
+// PATCH /todos/:id
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true})
+  .then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
   });
 });
 
