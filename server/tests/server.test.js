47656f544730b57859b37b520cdf28019b18f958
Austin Tinsley <austin1@live.unc.edu>
2018-03-12 18:25:12 -0400

    Add tests for POST /users/login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -207,7 +207,7 @@ describe('POST /users', () => {
           expect(user).to.exist;
           expect(user.password).to.not.equal(password);
           done();
-        })
+        }).catch((e) => done(e));
       });
   });
 
@@ -231,5 +231,56 @@ describe('POST /users', () => {
       })
       .expect(400)
       .end(done);
+  });
+});
+
+describe('POST /users/login', () => {
+  it('should login user and return auth token', (done) => {
+    request(app)
+      .post('/users/login')
+      .send({
+        email: users[1].email,
+        password: users[1].password
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.headers['x-auth']).to.exist;
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findById(users[1]._id).then((user) => {
+          expect(user.tokens[0]).to.include({
+            access: 'auth',
+            token: res.headers['x-auth']
+          });
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+
+  it('should reject invalid logins', (done) => {
+    request(app)
+      .post('/users/login')
+      .send({
+        email: users[1].email,
+        password: 'wrongPassword'
+      })
+      .expect(400)
+      .expect((res) => {
+        expect(res.headers['x-auth']).to.not.exist;
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findById(users[1]._id).then((user) => {
+          expect(user.tokens[0]).length.to.not.exist;
+          done();
+        }).catch((e) => done(e));
+      });
   });
 });
