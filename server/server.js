c834ac16108b3efa2009897443a6233b2e483387
Austin Tinsley <austin1@live.unc.edu>
2018-03-12 15:16:20 -0400

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -110,14 +110,28 @@ app.post('/users', (req, res) => {
   user.save().then(() => {
     return user.generateAuthToken();
   }).then((token) => {
-    res.header('x-auth', token).send(user);
+
   }).catch((e) => {
     res.status(400).send(e);
   });
 });
 
+//GET /users/me
 app.get('/users/me', authenticate, (req,res) => {
   res.send(req.user);
+});
+
+// POST /users/login
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((e) => {
+    res.status(400).send();
+  });
 });
 
 // Start server
